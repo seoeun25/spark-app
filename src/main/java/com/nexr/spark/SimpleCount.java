0b91f1bfb8b77ed124d829962783970eb2c86124
Azrael Park <azrael.park@nexr.com>
2015-02-27 08:16:58 +0900

    Add spark example

---
+++ src/main/java/com/nexr/spark/SimpleCount.java
@@ -0,0 +1,29 @@
+package com.nexr.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+
+public class SimpleCount {
+
+    public static void main(String... args) {
+        String logFile = "hdfs://seair.nexr.com:8020/user/seoeun/word-input";
+        SparkConf conf = new SparkConf().setAppName("Simple Application");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> logData = sc.textFile(logFile).cache();
+
+        long numAs = logData.filter(new Function<String, Boolean>() {
+            public Boolean call(String s) { return s.contains("a"); }
+        }).count();
+
+        long numBs = logData.filter(new Function<String, Boolean>() {
+            public Boolean call(String s) { return s.contains("b"); }
+        }).count();
+
+        System.out.println("Lines with a: " + numAs );
+
+        sc.stop();
+
+    }
+}
---
+++ src/main/java/com/nexr/spark/WordCount.java
@@ -0,0 +1,39 @@
+package com.nexr.spark;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
+import java.util.Arrays;
+
+public class WordCount {
+
+    public static void main(String... args) {
+        SparkConf conf = new SparkConf().setAppName("WordCount Application");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> file = sc.textFile("hdfs://seair.nexr.com:8020/user/seoeun/word-input");
+        JavaRDD<String> words = file.flatMap(new FlatMapFunction<String, String>() {
+            public Iterable<String> call(String s) {
+                return Arrays.asList(s.split(" "));
+            }
+        });
+        JavaPairRDD<String, Integer> pairs = words.mapToPair(new PairFunction<String, String, Integer>() {
+            public Tuple2<String, Integer> call(String s) {
+                return new Tuple2<String, Integer>(s, 1);
+            }
+        });
+        JavaPairRDD<String, Integer> counts = pairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
+            public Integer call(Integer a, Integer b) {
+                return a + b;
+            }
+        });
+        counts.saveAsTextFile("hdfs://seair.nexr.com:8020/user/seoeun/word-output");
+
+        sc.stop();
+    }
+}
